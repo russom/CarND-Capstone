128318cdc116119f09c0e11d5eede9d1497bc52b
Michelangelo Russo <michelangelo@Michelangelos-MacBook-Pro.local>
2020-05-30 11:46:23 +0200

    bug: removed typo in twist_cotroller.py

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -10,7 +10,7 @@ ONE_MPH = 0.44704
 
 class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius,
-                 wheel_base=, steer_ratio, max_lat_accel, max_steer_angle):
+                 wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
 
         # Yaw controller (pre-provided)
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
